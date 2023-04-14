20cc586f94f77ab65b4504c6b528ba52d5c58201
erinp0 <93521346+erinp0@users.noreply.github.com>
2023-04-14 13:16:15 +0100

    update

---
+++ Erin Pollard/adv_patch_job.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=adv_classifier_test_job
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=8:00:00
+#SBATCH --mem=20000M
+#SBATCH --account=math027744
+#SBATCH --error=adv_patch_error_gpu.txt
+#SBATCH --output=adv_patch_output_gpu.txt
+
+source activate
+
+conda activate tf-env
+
+cd "${SLURM_SUBMIT_DIR}"
+echo Running on host "$(hostname)"
+echo Time is "$(date)"
+echo Directory is "$(pwd)"
+echo Slurm job ID is "${SLURM_JOBID}"
+echo This jobs runs on the following machines:
+echo "${SLURM_JOB_NODELIST}"
+
+python Adversarial-Classifier-No-noisy.py
